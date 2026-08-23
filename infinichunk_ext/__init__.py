c276486103c1374f14bffd59960b269489685291
Divyam Talwar <divyamtalwar0@gmail.com>
2026-08-24 00:26:13 +0530

    feat: export the carryover research surface

--- infinichunk_ext/__init__.py
+++ infinichunk_ext/__init__.py
@@ -1,5 +1,12 @@
 """Original INFINICHUNK extensions layered over the attributed VERL fork."""
 
 from .prefix_cache import PrefixCacheReceipt, common_prefix_length
+from .carryover import CarryoverBenchmark, ContinuityVerifier, SalienceCarryoverSelector
 
-__all__ = ["PrefixCacheReceipt", "common_prefix_length"]
+__all__ = [
+    "CarryoverBenchmark",
+    "ContinuityVerifier",
+    "PrefixCacheReceipt",
+    "SalienceCarryoverSelector",
+    "common_prefix_length",
+]
